136ca2e8e2a1b739e4fc4e762e2e3790923c8c3f
w50005062 <wangyifan31@huawei.com>
2019-08-19 12:27:47 +0800

    新增webpack babel react相关配置

---
+++ src/search.js
@@ -0,0 +1,16 @@
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'css/search.css';
+
+class Search extends React.Component {
+  render () {
+    return <div> Hello webpack react</div>;
+  }
+}
+
+ReactDOM.render(
+  <Search />,
+  document.getElementById('root')
+);
